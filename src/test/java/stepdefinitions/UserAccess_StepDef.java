a61557ba2c25bdf85013e9fd8544783f3d58de84
Mohamed Nheri <nherimohamed@gmail.com>
2024-09-24 21:58:40 -0400

    new changes

--- src/test/java/stepdefinitions/UserAccess_StepDef.java
+++ src/test/java/stepdefinitions/UserAccess_StepDef.java
@@ -19,6 +19,7 @@ public class UserAccess_StepDef {
     WebDriver driver = Driver.getDriver();//declaring the driver as global because it will be used across all the class
     LoginPage loginPage = new LoginPage();
     SettingsPage settingsPage = new SettingsPage();
+
     @Given("user is navigated to Crater login page")
     public void user_is_navigated_to_crater_login_page() {
         driver.manage().window().maximize();
@@ -28,27 +29,26 @@ public class UserAccess_StepDef {
     }
     @When("user enters valid username and valid password")
     public void user_enters_valid_username_and_valid_password() throws InterruptedException {
-        SeleniumUtils.sendKeysUsingJavaScriptExecutor("entityadmin@primetechschool.com",loginPage.emailInput);
-        Thread.sleep(3000);
-        SeleniumUtils.sendKeysUsingJavaScriptExecutor("primetech@school",loginPage.passwordInput);
-        Thread.sleep(3000);
+        SeleniumUtils.sendkeysWithActionsClass(loginPage.emailInput, "entityadmin@primetechschool.com");
+        Thread.sleep(1000);
+        SeleniumUtils.sendkeysWithActionsClass(loginPage.passwordInput, "primetech@school");
+        Thread.sleep(1000);
     }
     @And("user clicks on login button")
     public void user_clicks_on_login_button() throws InterruptedException {
-        //loginPage.loginButton.click();
-        SeleniumUtils.clickUsingJavaScriptExecutor(loginPage.loginButton);
-        Thread.sleep(3000);
+        loginPage.loginButton.click();
+        Thread.sleep(1000);
     }
     @Then("user should be logged in successfully")
     public void user_should_be_logged_in_successfully() {
-//        //verify using the url that is different from login
-//        String loginUrl = "http://crater.primetech-apps.com/login";
-//        String afterLoginUrl = driver.getCurrentUrl();
-//        System.out.println("Current URL after login in is : " + afterLoginUrl);
-//        Assert.assertNotEquals(loginUrl , afterLoginUrl);
-//        //verify using the setting label is displayed
-//        Assert.assertTrue(settingsPage.settingsLabel.isDisplayed());
-
+        //verify using the url that is different from login
+        String loginUrl = "http://crater.primetech-apps.com/login";
+        String afterLoginUrl = driver.getCurrentUrl();
+        System.out.println("Current URL after login in is : " + afterLoginUrl);
+        Assert.assertNotEquals(loginUrl , afterLoginUrl);
+        //verify using the setting label is displayed
+        Assert.assertTrue(settingsPage.settingsLabel.isDisplayed());
+        Driver.closeDriver();
     }
 
 
@@ -56,15 +56,24 @@ public class UserAccess_StepDef {
     // Second scenario
 
     @When("user enters invalid username and valid password")
-    public void user_enters_invalid_username_and_valid_password() {
-
+    public void user_enters_invalid_username_and_valid_password() throws InterruptedException {
+        //entering invalid username and valid password
+        SeleniumUtils.sendkeysWithActionsClass(loginPage.emailInput, "invalid@primetechschool.com");
+        Thread.sleep(1000);
+        SeleniumUtils.sendkeysWithActionsClass(loginPage.passwordInput, "primetech@school");
+        Thread.sleep(1000);
     }
     @Then("user should see an error message {string} displayed")
-    public void user_should_see_an_error_message_displayed(String string) {
-
+    public void user_should_see_an_error_message_displayed(String expectedErrorMessage) {
+        //verify that the error message is displayed is equal to the expected error message
+        Assert.assertTrue(loginPage.loginErrorMessageLabel.getText().equals(expectedErrorMessage));
     }
     @And("user should not be logged in")
     public void user_should_not_be_logged_in() {
+        String loginUrl = "http://crater.primetech-apps.com/login";
+        //verify that login url didn't changes , meaning we are still in the same page and we couldn't login
+        Assert.assertTrue(driver.getCurrentUrl().equals(loginUrl));
+        Driver.closeDriver();//close the driver
 
     }
 
