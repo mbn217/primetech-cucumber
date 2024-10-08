d2add2956f9820c8bbbed34c8a38db1e081b6011
Mohamed Nheri <nherimohamed@gmail.com>
2024-10-07 20:19:05 -0400

    AddItem test case

--- src/test/java/utilities/SeleniumUtils.java
+++ src/test/java/utilities/SeleniumUtils.java
@@ -16,7 +16,9 @@ public class SeleniumUtils {
      * @param valueToBeSelected the option value that we want to select
      */
     public static void selectByValueFromDropDown(WebElement element, String valueToBeSelected){
+        //create an instance of the Select class
         Select select = new Select(element);
+        //select the option by value
         select.selectByValue(valueToBeSelected);
     }
 
@@ -28,26 +30,52 @@ public class SeleniumUtils {
      * @param targetElement the target element that we want to drag the source element to
      */
     public static void dragAndDrop(WebDriver driver , WebElement sourceElement, WebElement targetElement ){
+        //create an instance of the Actions class
         Actions actions = new Actions(driver);
+        //drag and drop the source element to the target element
         actions.dragAndDrop(sourceElement, targetElement).build().perform();
     }
 
+    /**
+     * This method will send keys to an element using the JavascriptExecutor
+     * @param inputString the input that we want to send to the element
+     * @param element the element that we want to send keys to
+     */
     public static void sendKeysUsingJavaScriptExecutor(String inputString , WebElement element){
+        //create an instance of the JavascriptExecutor
         JavascriptExecutor js  = (JavascriptExecutor) Driver.getDriver();
+        //send the keys to the element
         js.executeScript("arguments[0].setAttribute('value', '" + inputString +"')", element);
     }
 
+    /**
+     * This method will send keys to an element using the Actions class
+     * @param element the element that we want to send keys to
+     * @param input the input that we want to send to the element
+     */
     public static void sendkeysWithActionsClass(WebElement element , String input){
+        //create an instance of the Actions class
         Actions actions = new Actions(Driver.getDriver());
+        //send the keys to the element
         actions.sendKeys(element , input).build().perform();
     }
 
+    /**
+     * This method will check if the item is in the table
+     * @param elementList the list of elements that are in the table
+     * @param itemNameToSearchFor the item name that we are looking for
+     * @return true if the item is in the table, false if the item is not in the table
+     */
     public static boolean isItemInTable(List<WebElement> elementList , String itemNameToSearchFor){
+        //loop through the list of elements
         for(WebElement item : elementList){
+            //if the item text is equal to the item name that we are looking for
             if(item.getText().equals(itemNameToSearchFor)){
+                //return true if the item is in the table
                 return true;
             }
         }
+        //if the item is not in the table
         return  false;
     }
 
