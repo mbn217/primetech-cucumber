5883260d588571468b63540da5ecd885ea574f90
Mohamed Nheri <nherimohamed@gmail.com>
2024-11-04 21:38:56 -0500

    Add new invoice test case

--- src/test/java/api_tests/CraterAPIInvoices.java
+++ src/test/java/api_tests/CraterAPIInvoices.java
@@ -4,7 +4,9 @@ import io.restassured.RestAssured;
 import io.restassured.response.Response;
 import org.testng.annotations.Test;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class CraterAPIInvoices {
@@ -12,6 +14,7 @@ public class CraterAPIInvoices {
     String baseUrl = "http://crater.primetech-apps.com/";
     Response response;
     String token;
+    List<Integer> invoiceIds = new ArrayList<>();
 
     @Test
     public void loginToCraterApp(){
@@ -60,11 +63,57 @@ public class CraterAPIInvoices {
                 .get(baseUrl+endpoint);
 
         response.then().statusCode(200);
+        //response.prettyPrint();
+        invoiceIds = response.jsonPath().getList("data.id");
+        System.out.println(invoiceIds);
+
+    }
+
+    @Test(dependsOnMethods = "get_all_Invoices")
+    public void get_specific_Invoice(){
+        String endpoint = "api/v1/invoices/" +invoiceIds.get(0);
+        System.out.println("Full endpoint is " + baseUrl+endpoint);
+
+        Map<String, Object> requestHeaders = new HashMap<>();
+        requestHeaders.put("Content-Type", "application/json");
+        requestHeaders.put("Accept", "application/json");
+        requestHeaders.put("Authorization", "Bearer " + token);
+
+
+        response = RestAssured.given()
+                .headers(requestHeaders)
+                .when()
+                .get(baseUrl+endpoint);
+
+        response.then().statusCode(200);
+
         response.prettyPrint();
 
 
-
-
     }
+
+// We need access to perform a delete operation (admin user )
+//    @Test(dependsOnMethods = "get_specific_Invoice")
+//    public void delete_specific_Invoice(){
+//        String endpoint = "api/v1/invoices/" +invoiceIds.get(0);
+//        System.out.println("Full endpoint is " + baseUrl+endpoint);
+//
+//        Map<String, Object> requestHeaders = new HashMap<>();
+//        requestHeaders.put("Content-Type", "application/json");
+//        requestHeaders.put("Accept", "application/json");
+//        requestHeaders.put("Authorization", "Bearer " + token);
+//
+//
+//        response = RestAssured.given()
+//                .headers(requestHeaders)
+//                .when()
+//                .delete(baseUrl+endpoint);
+//
+//        response.then().statusCode(200);
+//
+//        response.prettyPrint();
+//
+//
+//    }
 
 }
