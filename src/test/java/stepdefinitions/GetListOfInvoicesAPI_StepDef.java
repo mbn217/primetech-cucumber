25578e0335cb4850294cfe46ff068eda08a51f3d
Mohamed Nheri <nherimohamed@gmail.com>
2024-11-05 19:16:59 -0500

    added api

---
+++ src/test/java/stepdefinitions/GetListOfInvoicesAPI_StepDef.java
@@ -0,0 +1,28 @@
+package stepdefinitions;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class GetListOfInvoicesAPI_StepDef {
+
+    @Given("Im logged in successfully")
+    public void im_logged_in_successfully() {
+
+    }
+    @When("I perform GET operation for {string} endpoint")
+    public void i_perform_get_operation_for_endpoint(String string) {
+
+    }
+    @Then("I should get {int} status code")
+    public void i_should_get_status_code(Integer int1) {
+
+    }
+    @And("I should get list of invoices")
+    public void i_should_get_list_of_invoices() {
+
+    }
+
+
+}
