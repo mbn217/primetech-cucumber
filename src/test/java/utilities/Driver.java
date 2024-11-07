debddc9dd4b4965f8c0cb7fbd871717c7eb8df9f
MohamedNheri(US) <nherimohamed@gmail.com>
2024-11-07 13:43:08 -0500

    saucelabs example

--- src/test/java/utilities/Driver.java
+++ src/test/java/utilities/Driver.java
@@ -6,7 +6,13 @@ import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.safari.SafariDriver;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Driver {
 
@@ -25,7 +31,7 @@ public class Driver {
         //Read the browser type you want to launch from properties file
         String browserType = ConfigurationReader.getPropertyValue("browserType");
 
-        if(driver == null){//if the driver is null, then create the driver
+        if(driver == null || ((RemoteWebDriver) driver).getSessionId() == null){//if the driver is null, then create the driver
             //Create the driver based on the browser type
             switch (browserType){
                 case "chrome"://if the browser type is chrome
@@ -49,6 +55,25 @@ public class Driver {
                     FirefoxOptions firefoxOptions = new FirefoxOptions();
                     firefoxOptions.addArguments("--headless");
                     driver = new FirefoxDriver(firefoxOptions);
+                    break;
+                case "sauceLabs":
+                    ChromeOptions browserOptions = new ChromeOptions();
+                    browserOptions.setPlatformName("Windows 11");
+                    browserOptions.setBrowserVersion("latest");
+                    Map<String, Object> sauceOptions = new HashMap<>();
+                    sauceOptions.put("username", "oauth-nherimohamed-5376a");
+                    sauceOptions.put("accessKey", "5793ba5b-544d-4255-b4a9-ea0b94ee8d86");
+                    sauceOptions.put("build", "selenium-build-CJ6LS");
+                    sauceOptions.put("name", "<your test name>");
+                    browserOptions.setCapability("sauce:options", sauceOptions);
+                    // start the session
+                    URL url = null;
+                    try {
+                        url = new URL("https://ondemand.us-west-1.saucelabs.com:443/wd/hub");
+                    } catch (MalformedURLException e) {
+                        throw new RuntimeException(e);
+                    }
+                    driver = new RemoteWebDriver(url, browserOptions);
                     break;
                 default:
                     driver = new ChromeDriver(options);
