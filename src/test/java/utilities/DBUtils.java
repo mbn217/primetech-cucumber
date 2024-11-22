00803696500c6963f8a5861b5321077a5045aa34
MohamedNheri(US) <nherimohamed@gmail.com>
2024-11-22 11:23:46 -0500

    small changes/updates

--- src/test/java/utilities/DBUtils.java
+++ src/test/java/utilities/DBUtils.java
@@ -9,7 +9,10 @@ public class DBUtils {
     static String userName = "craterdbuser";
     static String password = "ptschool2023";
 
-
+    /**
+     * This method will create connection to the database
+     * @return Connection object
+     */
     public static Connection getConnection(){
         try {
             return DriverManager.getConnection(dbUrl, userName, password);
@@ -17,6 +20,14 @@ public class DBUtils {
             throw new RuntimeException(e);
         }
     }
+
+    /**
+     * This method will execute the query and return the data set
+     * This method will return the data set as a list of lists meaning each row will be a list of strings
+     * It will return all the data from the query
+     * @param query the query to execute
+     * @return List of List of Strings
+     */
     public  static List<List<String>> executeQuery(String query){
         List<List<String>> dataSet = new ArrayList<>();
         try {
@@ -51,6 +62,14 @@ public class DBUtils {
 
 
     //Select query that will return the first record as a list of strings
+
+    /**
+     * This method will execute the query and return the data set
+     * This method will return a single record as a list of strings
+     * If you have multiple columns in the query it will return the first row only
+     * @param query the query to execute
+     * @return List of Strings
+     */
     public  static List<String> selectRecord(String query){
         List<String> dataSet = new ArrayList<>();
         try {
@@ -79,7 +98,14 @@ public class DBUtils {
     }
 
 
-    //Will select the first record and return back the data for that specific column
+
+    /**
+     * This method will execute the query and return the data set
+     * It will return a single record as a string , you will provide the column name and it will return the data for that column
+     * @param query the query to execute
+     * @param colName the column name to get the data from
+     * @return
+     */
     public  static String selectRecord(String query, String colName){
         String  dataSet  = null;
         try {
@@ -104,13 +130,6 @@ public class DBUtils {
         }
         return dataSet;
     }
-
-
-
-
-
-
-
 
 
 }
